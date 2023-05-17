975159b554179cd44ebb260a60659a51ecd9d9f6
Raquel Adrados <radrados00@gmail.com>
2023-05-17 22:26:24 +0200

    Initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
 export default {
-  preset: "ts-jest",
-  testPathIgnorePatterns: ["dist"],
-  resolver: "jest-ts-webcompat-resolver",
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['dist'],
+  resolver: 'jest-ts-webcompat-resolver',
 };
